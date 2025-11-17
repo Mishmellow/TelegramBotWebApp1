f00de7dab9c049587b5e1b7a10fd8c89a45f885a
Misha <guglmit599@gmail.com>
2025-11-17 22:21:54 +0200

    Final attempt to disable BOT_TOKEN check

--- settings.py
+++ settings.py
@@ -1,7 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не найдет в переменных среды!")
 
 MANAGER_CHAT_ID = int(os.getenv('MANAGER_CHAT_ID', 0))
