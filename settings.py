d778c422f9e59b899db70fda08e0cc05144f4e2c
Misha <guglmit599@gmail.com>
2025-11-19 20:08:23 +0200

    Fix: Set WEBHOOK_HOST using railway.yaml to ensure variable resolution

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MANAGER_CHAT_ID = int(os.getenv('MANAGER_CHAT_ID', 0))
 
-WEBHOOK_HOST = os.getenv('WEBHOOK_HOST', 'http://localhost')
+WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
