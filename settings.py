f623f825bbfca5f442a6117a673a8d22e3f28419
Misha <guglmit599@gmail.com>
2025-12-05 20:53:33 +0200

    Fix .env and settings

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 import os
-
 from dotenv import load_dotenv
-load_dotenv()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MANAGER_CHAT_ID = int(os.getenv('MANAGER_CHAT_ID', 0))
-
 WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
 WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
