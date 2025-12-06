92bcb1adf650fe12d6a15c9a68da47c42c5e104c
Misha <guglmit599@gmail.com>
2025-12-06 19:06:10 +0200

    Fix from admin import router

--- app/start.py
+++ app/start.py
@@ -1,9 +1,12 @@
-import json
 import logging
+import asyncio
 from aiogram import Bot, Dispatcher, Router, F
 from aiogram.types import Message, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.state import State, StatesGroup
-from settings import WEBAPP_URL, BOT_TOKEN
+
+from settings import WEBAPP_URL, BOT_TOKEN, MANAGER_CHAT_ID
+from api_service import set_bot_instance
+from admin import admin_router
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +21,9 @@ class UserForm(StatesGroup):
 async def command_start_handler(message: Message) -> None:
 
     web_app_url = WEBAPP_URL
+
     web_app_info = WebAppInfo(url=web_app_url)
+
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="🚀 Перейти в Web App", web_app=web_app_info)]
     ])
@@ -26,8 +31,25 @@ async def command_start_handler(message: Message) -> None:
     await message.answer("Добро пожаловать! Нажмите кнопку ниже, чтобы открыть наше Web App:", reply_markup=keyboard)
 
 
-def initialize_bot() -> tuple[Bot, Dispatcher]:
-    bot = Bot(token=BOT_TOKEN)
+def initiate_bot() -> tuple[Bot, Dispatcher]:
+    bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
     dp = Dispatcher()
+
     dp.include_router(router)
+    dp.include_router(admin_router)
+
+    set_bot_instance(bot, MANAGER_CHAT_ID)
+
     return bot, dp
+
+
+async def main():
+    bot, dp = initiate_bot()
+
+    logger.info("Bot Polling service started.")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
